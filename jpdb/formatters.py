ca0c89ca34eb24377a821d5b0e4b3e930a973b0e
Amachoma <as.derbin99@gmail.com>
2024-06-07 19:06:45 +0900

    jpdb words parser

--- jpdb/formatters.py
+++ jpdb/formatters.py
@@ -1,15 +1,26 @@
 from bs4 import BeautifulSoup
 
-from utils.file import open_lines
+from utils.file import open_lines, write_line
 
 if __name__ == "__main__":
-    kanji_dataset = open_lines("./out/raw_pages.txt")
+    kanji_dataset = open_lines("out/raw_pages_all.txt")
+
+    url_to_process = set()
     for record in kanji_dataset:
         [examples, vocab, kanji] = [record["pages"]['examples'], record["pages"]['vocab'], record["pages"]['kanji']]
 
         examples_section = BeautifulSoup(examples, "html.parser").find(attrs={'class': 'subsection-examples'})
-        vocab_section = BeautifulSoup(examples, "html.parser").find(attrs={'class': 'subsection-used-in'})
-        kanji_section = BeautifulSoup(examples, "html.parser").find(attrs={'class': 'subsection-composed-of-kanji'})
+        vocab_section = BeautifulSoup(vocab, "html.parser").find(attrs={'class': 'subsection-used-in'})
+        kanji_section = BeautifulSoup(kanji, "html.parser").find(attrs={'class': 'subsection-composed-of-kanji'})
 
-        print(kanji_section)
-        break
+        vocab_tags = vocab_section.findAll(attrs={'class': 'used-in'})
+        for tag in vocab_tags:
+            url_split = [x.encode("latin-1").decode("utf-8") for x in tag.find("a").get("href").split("/")[2:-1]]
+            word_id = int(url_split[0])
+            converted_url = "/".join(url_split)
+            word_url = f"https://jpdb.io/vocabulary/{converted_url}/used-in"
+            url_to_process.add(word_url)
+
+    print(len(url_to_process))
+    for url in url_to_process:
+        write_line("./out/vocab_links", url)
--- jpdb/parser.py
+++ jpdb/parser.py
@@ -1,12 +1,15 @@
 import requests
+import asyncio
 from time import time
+from bs4 import BeautifulSoup
 
 from utils.file import open_json, open_lines, write_line
 from utils.time import AvgRemainingTime
 from utils.print_utils import draw_progress_bar
 
-if __name__ == "__main__":
-    downloaded_data = [record['character'] for record in open_lines('./out/raw_data.txt')]
+
+def download_kanji_pages():
+    downloaded_data = [record['character'] for record in open_lines('out/raw_pages_all.txt')]
 
     levels = open_json("../levels.json")
 
@@ -17,17 +20,66 @@ if __name__ == "__main__":
         level = levels[index]
         for kanji_index in range(len(level['kanji'])):
             kanji = level['kanji'][kanji_index]
-            if kanji['character'] in downloaded_data:
+            if kanji['character'] in downloaded_data or kanji['character'] == "々":
                 continue
 
             print(f"Processing {kanji['character']}")
             start_time = time()
 
-            page = requests.get(f"https://jpdb.io/vocabulary/1629200/{kanji['character']}/used-in")
-            write_line('./out/raw_data.txt', {"html": page.text, "character": kanji['character']})
+            pages = {}
+            sections = {'examples': 'e', 'vocab': 'v', 'kanji': 'k'}
+            for [key, value] in sections.items():
+                url = f"https://jpdb.io/kanji/{kanji['character']}?expand={value}#used_in_{kanji['character']}_{value}"
+                page = requests.get(url)
+                pages[key] = page.text
+            write_line('out/raw_pages_all.txt', {"pages": pages, "character": kanji['character']})
 
             elapsed_time = time() - start_time
             avg_timer.add_item(elapsed_time)
 
             draw_progress_bar(avg_timer.percent)
             print(f"{avg_timer}\n\n\n")
+
+
+# def process_page(page):
+#     soup = BeautifulSoup(page, "html.parser")
+#     frequency_table_tag = soup.find("table", attrs={'class': ['cross-table', 'label-right-align']})
+#     print(frequency_table_tag)
+
+
+def download_word_pages():
+    word_links = open_lines("./out/vocab_links")
+
+    finished_word_ids = [x["word_id"] for x in open_lines("./out/words_all.txt")]
+
+    total_count = len(word_links) - len(finished_word_ids)
+
+    avg_timer = AvgRemainingTime(total=total_count)
+
+    for link in word_links:
+        word_id = int(link.split("/")[-3])
+        if word_id in finished_word_ids:
+            continue
+
+        print(f"Processing link '{link}'... ")
+        start_time = time()
+
+        page = requests.get(link)
+        soup = BeautifulSoup(page.text, "html.parser")
+        write_line("./out/words_all.txt", {"page": str(soup.find("body")), "word_id": word_id})
+
+        elapsed_time = time() - start_time
+        avg_timer.add_item(elapsed_time)
+
+        draw_progress_bar(avg_timer.percent)
+        print(f"{avg_timer}\n\n\n")
+
+
+if __name__ == "__main__":
+    download_word_pages()
+    # download_kanji_pages()
+    # pages = open_lines("./out/raw_data.txt")
+    # for page in pages:
+    #     print(f"processing {page['character']}")
+    #     process_page(page['html'])
+    #     break
